11aaeaba60ba6133d6265944581575b5eb8e46a3
Christian Awelakoue <awelakoue@gmail.com>
2019-06-22 14:41:44 +0200

    Add Publisher Entity

--- src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.example.spring5webapp.bootstrap;
 
 import com.example.spring5webapp.model.Author;
 import com.example.spring5webapp.model.Book;
+import com.example.spring5webapp.model.Publisher;
 import com.example.spring5webapp.repositories.AuthorRepository;
 import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,9 +29,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        Publisher publisher = new Publisher();
+        publisher.setName("Foo");
+
+        publisherRepository.save(publisher);
+
         //Maya Angelou
         Author maya = new Author("Maya", "Angelou");
-        Book caged = new Book("I know why the caged bird sings", "1234", "unknown");
+        Book caged = new Book("I know why the caged bird sings", "1234", publisher);
         maya.getBooks().add(caged);
         caged.getAuthors().add(maya);
 
@@ -36,7 +45,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Chinua Achebe
         Author chi = new Author("Chinua", "Achebe");
-        Book things = new Book("Things fall apart", "123456", "Worx");
+        Book things = new Book("Things fall apart", "123456", publisher);
         chi.getBooks().add(things);
 
         authorRepository.save(chi);
